94ef522b9d9acd49d0b0d2da809e7382da068187
Aivan Ross Anuyo <aish3n@pm.me>
2023-11-26 11:17:57 +0800

    fix(main): stop stringifying MESSAGE parameter in macros

--- include/main.h
+++ include/main.h
@@ -12,13 +12,13 @@
 
 #define GOTO_IF_TRUE(LABEL, EXPRESSION, MESSAGE, ...) do {\
 		if (EXPRESSION) {\
-			fprintf(stderr, #MESSAGE, ##__VA_ARGS__);\
+			fprintf(stderr, MESSAGE, ##__VA_ARGS__);\
 			goto LABEL;\
 		}\
 	} while (false)
 
 #define GOTO_IF_FAILURE(LABEL, STATEMENT, MESSAGE, ...) \
-	GOTO_IF_TRUE(LABEL, STATEMENT != MP2A_SUCCESS, #MESSAGE, ##__VA_ARGS__);
+	GOTO_IF_TRUE(LABEL, STATEMENT != MP2A_SUCCESS, MESSAGE, ##__VA_ARGS__);
 
 #define GOTO_IF_ALLOC_NULL(LABEL, IDENTIFIER, ALLOC_STATEMENT) do {\
 		IDENTIFIER = ALLOC_STATEMENT;\
@@ -31,12 +31,12 @@
 
 #define FAIL_IF_TRUE(EXPRESSION, MESSAGE, ...) do {\
 		if (EXPRESSION) {\
-			fprintf(stderr, #MESSAGE, ##__VA_ARGS__);\
+			fprintf(stderr, MESSAGE, ##__VA_ARGS__);\
 			return MP2A_FAILURE;\
 		}\
 	} while (false)
 
-#define FAIL_IF_FAILURE(STATEMENT, MESSAGE, ...) FAIL_IF_TRUE(STATEMENT != MP2A_SUCCESS, #MESSAGE, ##__VA_ARGS__);
+#define FAIL_IF_FAILURE(STATEMENT, MESSAGE, ...) FAIL_IF_TRUE(STATEMENT != MP2A_SUCCESS, MESSAGE, ##__VA_ARGS__);
 #define FAIL_IF_NULL(IDENTIFIER) FAIL_IF_TRUE(IDENTIFIER == NULL, "error: " #IDENTIFIER " is NULL.\n");
 
 
